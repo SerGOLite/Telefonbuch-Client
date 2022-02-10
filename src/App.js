fb08c1daaf1b1e940907d6fbaf8fbe0666a8af0d
Jeremias Binder <jeremias.binder@adesso.de>
2022-02-10 17:14:59 +0100

    Revert "fixed errors"
    This reverts commit 26a7cb90a15ea3009fa2bc275975a1aadcd481a7.

--- src/App.js
+++ src/App.js
@@ -6,19 +6,19 @@ import Toolbar from "@mui/material/Toolbar";
 import AppBar from "@mui/material/AppBar";
 import PhoneIcon from "@mui/icons-material/Phone";
 
+import { Typography, IconButton } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+
 import "./components/table";
 
 import InfoDisplay from "./components/infoDisplay";
-import Lettering from "./components/AppBarItems/Lettering";
 import Badges from "./components/AppBarItems/Badges";
 import SearchField from "./components/AppBarItems/SearchField";
 import AddIconButton from "./components/AppBarItems/AddIconButton";
-import MyMenuIcon from "./components/AppBarItems/MenuIcon";
 
 function App() {
   const [rows, setRows] = useState([]);
   const [filteredRows, setFilteredRows] = useState([]);
-  // const [suchbegriff, setSuchbegriff] = useState("");
 
   const updateSuchbegriff = (suchbegriff) => {
     setFilteredRows(
@@ -30,58 +30,54 @@ function App() {
       )
     );
   };
-
   const addPerson = async (name, adresse, telefonnummer) => {
     const newPerson = {
       name: name,
       adresse: adresse,
       telefonnummer: telefonnummer,
     };
-
     // wenn das backend ein ist, folgenden code auskommentieren
-    // setRows(rows.concat([newPerson]));
+    setRows(rows.concat([newPerson]));
 
     // Wenn das backend an ist, folgenden Code einkommentieren
-
-    await fetch("http://localhost:8082/api/person/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-
-      body: JSON.stringify(newPerson),
-    }).then((res) => {
-      res.json().then((data) => {
-        setRows(rows.concat([data]));
-        setFilteredRows(rows.concat([data]));
-      });
-    });
+    // setFilteredRows(rows.concat([data]));wait fetch("http://localhost:8082/api/person/", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(newPerson),
+    // }).then((res) => {
+    //   res.json().then((data) => {
+    //     setRows(rows.concat([data]));
+    //     setFilteredRows(rows.concat([data]));
+    //   });
+    // });
   };
 
   React.useEffect(() => {
     async function getData() {
       // wenn backend ein ist, folgenden Code auskommenteiren
-      // const person = {
-      //   name: "Person Name ",
-      //   adresse: "Person Adresse ",
-      //   telefonnummer: "123 + ",
-      // };
-      // const data = ["Alfred", "Ida", "Georg", "Sascha", "Jerry", "Sergej"].map(
-      //   (name) => ({ ...person, name: name })
-      // );
-
-      // setRows(data);
+      const person = {
+        name: "Person Name ",
+        adresse: "Person Adresse ",
+        telefonnummer: "123 + ",
+      };
+      const data = ["Alfred", "Ida", "Georg", "Sascha", "Jerry", "Sergej"].map(
+        (name) => ({ ...person, name: name })
+      );
+      setRows(data);
+      setFilteredRows(data);
 
       //wenn backend ein ist, folgenden Code einkommentieren
-      fetch("http://localhost:8082/api/person/", {
-        method: "GET",
-      }).then((res) => {
-        console.log("1", res);
-        res.json().then((data) => {
-          console.log("data", data, null, 2);
-          setRows(data);
-        });
-      });
+      // fetch("http://localhost:8082/api/person/", {
+      //   method: "GET",
+      // }).then((res) => {
+      //   console.log("1", res);
+      //   res.json().then((data) => {
+      //     console.log("data", data, null, 2);
+      //     setRows(data);
+      //   });
+      // });
     }
     getData();
   }, []);
@@ -97,37 +93,44 @@ function App() {
     <>
       <AppBar position="static">
         <Toolbar>
-          {/* <MyMenuIcon /> */}
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
           <PhoneIcon sx={{ mr: 4 }} />
-          {/* <Lettering /> */}
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+          >
+            Telefonbuch
+          </Typography>
           <AddIconButton addPerson={addPerson} />
 
           <Badges
             rows={rows}
             filteredRows={filteredRows}
-            filterApplied={false}
+            filterApplied={filteredRows.length !== rows.length}
           />
-          <SearchField setSuchbegriff={setSuchbegriff} />
+          <SearchField setSuchbegriff={updateSuchbegriff} />
         </Toolbar>
       </AppBar>
       <InfoDisplay
         rows={rows}
         filteredRows={filteredRows}
-        filterApplied={suchbegriff.length > 0}
+        filterApplied={filteredRows.length !== rows.length}
       />
-      {/* <Typography className="schrift">Test mit ClassName</Typography>
+      <Typography className="schrift">Test mit ClassName</Typography>
       <Typography sx={{ color: "blue" }}>Test mit Sx</Typography>
-      <div style={{ color: "blue" }}>Test mit styles</div> */}
+      <div style={{ color: "blue" }}>Test mit styles</div>
       <div>
-        <Table
-          rows={rows.filter(
-            (entry) =>
-              entry.name.toLowerCase().includes(suchbegriff) ||
-              entry.adresse.toLowerCase().includes(suchbegriff) ||
-              entry.telefonnummer.toLowerCase().includes(suchbegriff)
-          )}
-          deleteItems={deleteItems}
-        />
+        <Table rows={filteredRows} deleteItems={deleteItems} />
       </div>
     </>
   );
